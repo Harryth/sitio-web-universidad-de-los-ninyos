cf041295a3955bda52c5b6d377b1a47fe443ff61
Harold Vallejo <hyvallejom@gmail.com>
2014-04-22 22:44:37 -0500

    Primer desarrollo de una página de sección interna (estudiantes) implementeada funcionalidad en el menú de navegación superior con hojas de estilo separadas

--- js/nav-app.js
+++ js/nav-app.js
@@ -1,4 +1,14 @@
 $(function() {
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 120)
+            $("nav").css("top","0");
+        else
+            $("nav").css("top","-52px");
+
+        $("#dummy").text($(this).scrollTop());
+    });
+
     $(".menu").mouseenter(function() {
         $(".submenu").css("display","block");
     });
