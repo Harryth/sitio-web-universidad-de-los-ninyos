00c6907938112a5dbbdc8b158a75376e9de05762
Harold Vallejo <hyvallejom@gmail.com>
2014-05-07 18:09:23 -0500

    Abrir página de licencia en la misma ventana

--- php/footer.php
+++ php/footer.php
@@ -3,7 +3,7 @@
     <footer>
     	<p><a href="http://unal.edu.co/" target="_blanck">Universidad Nacional de Colombia</a></p>
     	<p><a href="http://www.ingenieria.unal.edu.co/" target="_blanck">Facultad de Ingenier&iacute;a</a></p>
-    	<p><a href="/license.php" target="_blanck">Licencia</a></p>
+    	<p><a href="/license.php">Licencia</a></p>
     </footer>
 _TEXT;
 ?>
