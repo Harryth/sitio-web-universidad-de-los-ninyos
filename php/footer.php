1790f743b288446a79be278534d87ee04e56fba0
Harold Vallejo <hyvallejom@gmail.com>
2014-05-07 22:58:23 -0500

    Añadido link del departamento cambio texto de licencia

--- php/footer.php
+++ php/footer.php
@@ -3,7 +3,8 @@
     <footer>
     	<p><a href="http://unal.edu.co/" target="_blanck">Universidad Nacional de Colombia</a></p>
     	<p><a href="http://www.ingenieria.unal.edu.co/" target="_blanck">Facultad de Ingenier&iacute;a</a></p>
-    	<p><a href="/license.php">Licencia</a></p>
+    	<p><a href="http://www.ingenieria.unal.edu.co/es/dependencias/departamentos/departamento-de-ingenieria-mecanica-y-mecatronica" target="_blanck">Departamento de Ingenier&iacute;a Mec&aacute;nica y Mecatr&oacute;nica</a></p>
+    	<p><a href="/license.php">T&eacute;rminos de Licencia</a></p>
     </footer>
 _TEXT;
 ?>
