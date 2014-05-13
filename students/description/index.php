758677a0f9a8d52cb29ab2b9c10cfa78cb9fd641
Harold Vallejo <hyvallejom@gmail.com>
2014-05-13 06:34:03 -0500

    Agregado contenido

--- students/description/index.php
+++ students/description/index.php
@@ -31,13 +31,16 @@ nav("aside");
 ?>
 
         <div id="content">
-        	<h1 style="padding: 30px 30px 10px 30px;">Lorem Ipsum</h1>
-            <p style="padding: 10px 30px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin pharetra vel mi sed varius. Curabitur interdum erat nec nisi porttitor fermentum. Phasellus eu mi mattis, viverra justo in, sagittis orci. Sed nibh purus, tincidunt et mauris nec, vulputate rhoncus mauris. Sed eget venenatis turpis. Duis imperdiet augue vel interdum iaculis. Phasellus at neque a dui pellentesque tristique id et turpis. Nam aliquet mi justo, vitae gravida sem lacinia eget. Morbi sapien odio, mattis at lobortis non, posuere vitae odio. Ut in pretium leo, non posuere ipsum. Duis ac lacinia leo, tempus tempor nibh. Cras placerat, metus id vulputate tempus, magna orci interdum tortor, non dapibus nisi massa quis quam</p>
-            <p style="padding: 10px 30px;">Donec nisl lectus, aliquam sollicitudin ullamcorper ac, dapibus ac nulla. Sed lacinia blandit ipsum, sed ornare urna volutpat tincidunt. Nullam sagittis erat sit amet velit sodales accumsan. Ut hendrerit dictum eros, sed elementum enim commodo in. Duis pulvinar lobortis dolor. Vivamus sit amet aliquet tellus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Mauris non hendrerit metus.</p>
-            <p style="padding: 10px 30px;">Phasellus pellentesque euismod turpis quis commodo. Aenean sapien urna, tempor eget augue ac, aliquam aliquam urna. Suspendisse at dictum massa. In tempor dictum tempor. Sed rutrum tempor sapien et vehicula. Morbi in ullamcorper massa. Donec molestie accumsan turpis at varius. Suspendisse ut erat nec ligula dapibus molestie.</p>
-            <p style="padding: 10px 30px;">Etiam quis neque posuere eros pulvinar dictum. Proin sollicitudin est lorem, sed ullamcorper leo hendrerit sit amet. Duis at lacus ante. Fusce eget ornare magna. Donec in lacus id dui consequat imperdiet. Quisque convallis aliquet auctor. Duis hendrerit mauris ut vulputate ultrices. Quisque scelerisque dapibus tempor. Quisque feugiat leo eget metus lobortis, eu auctor nisl tincidunt.</p>
-            <p style="padding: 10px 30px;">Aliquam erat volutpat. Integer tempor nibh metus, at vehicula mi rutrum nec. Donec dapibus non orci ut aliquam. Maecenas faucibus a risus sit amet tempus. Donec consectetur massa nec urna dictum, ut ornare leo porttitor. Integer quis lacinia diam. Phasellus dictum mi erat, non cursus odio malesuada vel. Ut posuere sapien nec enim ultrices lacinia. Nam euismod eros et ligula gravida aliquam. Nullam nec urna ut magna dignissim tristique ut ac leo. Curabitur sit amet risus purus.</p>
+        	<h1>La Universidad de los Ni&ntilde;os</h1>
+        	<p>Actualmente la tecnolog&iacute;a hace parte fundamental de nuestras vidas, la podemos encontrar en casi todo, y las nuevas generaciones, como t&uacute;, han crecido con ella. La educaci&oacute;n tambi&eacute;n ha sido testigo de ese cambio y se adapta para hacer uso de estas ayudas tecnol&oacute;gicas para hacer m&aacute;s efectiva la ense&ntilde;anza y garantizar un proceso de aprendizaje m&aacute;s personalizado.</p>
+        	<p>La rob&oacute;tica, es un &aacute;rea tecnol&oacute;gica encargada del estudio de los robots  (sistemas compuestos por mecanismos que le permiten hacer movimientos y realizar tareas especificas, programables y eventualmente inteligentes) y es una de las &aacute;reas que cuenta con mayor crecimiento y auge en la actualidad, sin duda, para muchos de nosotros puede resultar bastante atractiva.</p>
+        	<p>Este proyecto existe con la finalidad de acercar a los estudiantes a estos nuevos desarrollos tecnol&oacute;gicos y ense&ntilde;arle los fundamentos cient&iacute;ficos de una manera sencilla, clara y por supuesto, &iexcl;divertida!.</p>
         </div>
+        
+        <div id="top-btn"><a href="#"></a></div>
+<?php
+require_once '../../php/footer.php';
+?>
 
     </body>
 </html>
