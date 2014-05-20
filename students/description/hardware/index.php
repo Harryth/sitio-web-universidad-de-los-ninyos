5145a087e6ff8bf506064d6c83a9e4df593f85be
Harryth <harryth.asgard@gmail.com>
2014-05-20 10:50:42 -0500

    Agregada descripción de hardware

--- students/description/hardware/index.php
+++ students/description/hardware/index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="es" class="sie">
+<html lang="es" class="students">
 	<head>
 		<meta charset="utf-8">
 
@@ -7,8 +7,8 @@
 		Remove this if you use the .htaccess -->
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-		<title>Acerca de - La Universidad de los Ni&ntilde;os</title>
-		<meta name="description" content="SIEBOT">
+		<title>Estudiantes &gt; Descripci&oacute;n &gt; Hardware - La Universidad de los Ni&ntilde;os</title>
+		<meta name="description" content="Descripci&oacute;n Hardware La Universidad de los Ni&ntilde;os">
 		<meta name="author" content="Harold Vallejo">
 
 		<meta name="viewport" content="width=device-width; initial-scale=1.0">
@@ -17,16 +17,17 @@
 		<link rel="shortcut icon" href="/favicon.ico">
 		<link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
-		<link rel="stylesheet" type="text/css" href="../../css/sections-layout.css" />
-		<link rel="stylesheet" type="text/css" href="../../css/sections-style.css" />
-		<link rel="stylesheet" type="text/css" href="../../css/nav-layout.css" />
-		<link rel="stylesheet" type="text/css" href="../../css/nav-style.css" />
-		<link rel="stylesheet" type="text/css" href="../css/sie.css" />
+		<link rel="stylesheet" type="text/css" href="../../../css/sections-layout.css" />
+		<link rel="stylesheet" type="text/css" href="../../../css/sections-style.css" />
+		<link rel="stylesheet" type="text/css" href="../../../css/nav-layout.css" />
+		<link rel="stylesheet" type="text/css" href="../../../css/nav-style.css" />
+		<!--<link rel="stylesheet" type="text/css" href="./css/style-sheet.css" />-->
+		<link rel="stylesheet" type="text/css" href="../../css/students.css" />
 		<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
 
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
-		<script src="../../js/nav-app.js"></script>
-		<script src="../../js/sections-app.js"></script>
+		<script src="../../../js/nav-app.js"></script>
+		<script src="../../../js/sections-app.js"></script>
 		
 		<script src="/js/jquery.js" type="text/javascript" charset="utf-8"></script>
 		<link rel="stylesheet" href="/css/prettyPhoto.css" type="text/css" media="screen" charset="utf-8" />
@@ -36,20 +37,29 @@
 
 	<body>
 <?php
-require_once '../../php/header.php';
+require_once '../../../php/header.php';
 ?>
 
-		<div id="breadcrumb"><a href="../">La Universidad de los Ni&ntilde;os</a> &gt; <a href="./">SIEBOT</a></div>
+		<div id="breadcrumb">
+			<a href="/students">Estudiantes</a> &gt; <a href="/students/description/">Descripci&oacute;n</a> &gt;  <a href="./">Hardware</a>
+		</div>
 
 <?php
-require_once '../../php/nav.php';
+require_once '../../../php/nav.php';
 nav("top");
 nav("aside");
 ?>
+
+		<!--<aside id="tabs" class="students">
+			<ul>
+				<li><span></span><a href="#"></a></li>
+				<li><span></span><a href="#"></a></li>
+			</ul>
+		</aside>-->
 		<div id="content">
 			<h1>SIEBOT</h1>
-			<p>SIEBOT es una plataforma rob&oacute;tica desarrollada con el fin de ense&ntilde;ar los principios de la rob&oacute;tica m&oacute;vil a ni&ntilde;os en edad escolar.</p>
-			<p>A continuaci&oacute;n encontraremos una galer&iacute;a con algunas im&aacute;genes de la plataforma SIEBOT.</p>
+			<p>SIEBOT es una plataforma de rob&oacute;tica m&oacute;vil, con el que podr&aacute;s aprender los principios fundamentales de esta ciencia, como: la programaci&oacute;n de hardware, la lectura de sensores, los principios de locomoci&oacute;n y movimiento, entre otros. SIEBOT es atr&aacute;ctivo, f&aacute;cil de usar y est&aacute; dise&ntilde;ado para que lo quieras usar cada vez m&aacute;s. Lo importante es que te diviertas mientras aprendes!</p>
+			<p>A continuaci&oacute;n encontrar&aacute;s unas im&aacute;genes para que conozcas el SIEBOT.</p>
 			<div class="gallery">
 				<a href="/images/SIEBOT/front.png" rel="prettyPhoto[siebot]" title="Vista Frontal."><img src="/images/thumbnails/t-front.png" width="120" height="120" alt="Vista Frontal SIEBOT" /></a>
 				<a href="/images/SIEBOT/rigth.png" rel="prettyPhoto[siebot]" title="Vista Frontal-Derecha."><img src="/images/thumbnails/t-rigth.png" width="120" height="120" alt="Vista Frontal-Derecha SIEBOT" /></a>
@@ -58,14 +68,15 @@ nav("aside");
 				<a href="/images/SIEBOT/no-cover.png" rel="prettyPhoto[siebot]" title="Vista Interior."><img src="/images/thumbnails/t-no-cover.png" width="120" height="120" alt="Vista Interior SIEBOT" /></a>
 			</div>
 		</div>
-		
+
 		<div id="top-btn">
 			<a href="#"></a>
 		</div>
 
 <?php
-require_once '../../php/footer.php';
+require_once '../../../php/footer.php';
 ?>
+
 		<script type="text/javascript" charset="utf-8">
 		  $(document).ready(function(){
 		    $("a[rel^='prettyPhoto']").prettyPhoto();
